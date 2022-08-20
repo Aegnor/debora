9a4c71dc5064a843ed30f13b025fcf220426d025
OldBoy <orionblankoff@gmail.com>
2022-08-20 13:10:01 +0300

    added DOMContentLoaded event

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@
 import './styles/app.scss';
 
 // Components
-import './components/music-player';
+import AudioPlayer from './components/AudioPlayer';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const audioPlayer = new AudioPlayer('.js-player');
+  audioPlayer.init();
+});
